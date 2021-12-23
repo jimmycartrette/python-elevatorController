83df54a4594204cc7aadabe46c8fa0ab320f75da
Jimmy Cartrette <jimmy.cartrette@gmail.com>
2021-12-23 16:45:14 -0500

    timer starts http orchestrator

--- elevatorControllerTimerStart/__init__.py
+++ elevatorControllerTimerStart/__init__.py
@@ -1,14 +1,9 @@
-import datetime
-import logging
-
+import requests
+import os
 import azure.functions as func
+import azure.durable_functions as df
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    url = os.environ['ELEVATORSTARTERURL']
+    r = requests.get(url)
