d64ea7929e3e33be40bdc6d8c4e71892df910bd0
Jimmy Cartrette <jimmy.cartrette@gmail.com>
2021-12-23 22:03:19 -0500

    elevator state in cosmos

--- elevatorControllerOrchestrator/__init__.py
+++ elevatorControllerOrchestrator/__init__.py
@@ -9,11 +9,19 @@ from random import randint, random
 import enum
 import logging
 import math
-import json
+import os
 from azure.messaging.webpubsubservice import WebPubSubServiceClient
 from azure.core.credentials import AzureKeyCredential
 import azure.functions as func
 import azure.durable_functions as df
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.exceptions as exceptions
+
+COSHOST = os.environ['COSHOST']
+COSMASTER_KEY = os.environ['COSMASTER_KEY']
+COSDATABASE_ID = os.environ['COSDATABASE_ID']
+NUMBER_OF_ELEVATORS = 4
+NUMBER_OF_FLOORS = 6
 
 
 class ElevatorDirection(enum.IntEnum):
@@ -29,25 +37,29 @@ class ElevatorStatus(enum.IntEnum):
 def orchestrator_function(context: df.DurableOrchestrationContext):
     service = WebPubSubServiceClient(endpoint='https://crazyelevator.webpubsub.azure.com',
                                      hub='elevator', credential=AzureKeyCredential("sx3EtHYQQxPZiAZuHYDiEkDCflnI24Q00M7MJmCCg1k="))
+    client = cosmos_client.CosmosClient(COSHOST, {'masterKey': COSMASTER_KEY})
+    elevatorsDB = client.get_database_client(database="elevatorSystem")
 
-    elevatorStates = []
-    for i in range(4):
-        elevatorStates.append(
-            {"elevatorNumber": i+1, "elevatorStatus": ElevatorStatus.ATFLOOR, "atFloor": 1, "key": i})
+    elevatorsContainer = elevatorsDB.get_container_client(container="elevator")
+    elevatorsQuery = "SELECT * FROM c WHERE c.elevatorNumber<=" + \
+        str(NUMBER_OF_ELEVATORS)
+    elevatorStates = list(elevatorsContainer.query_items(
+        query=elevatorsQuery, enable_cross_partition_query=True))
     elevatorDoorStates = []
-    for i in range(6*4):
+    for i in range(NUMBER_OF_FLOORS*NUMBER_OF_ELEVATORS):
         elevatorDoorStates.append({"elevatorShaftNumber": i %
-                                   4+1, "floor": 6-math.floor(i/4), "open": False, "key": i+3})
+                                   NUMBER_OF_ELEVATORS+1, "floor": NUMBER_OF_FLOORS-math.floor(i/NUMBER_OF_ELEVATORS), "open": False, "key": i+3})
     state = {"elevatorState": elevatorStates,
              "elevatorDoorState": elevatorDoorStates}
-    whichelevator = randint(1, 4)
-    whichfloor = randint(1, 6)
-    elevatorStates[whichelevator-1]["atFloor"] = whichfloor
+    whichelevatorIndex = randint(0, NUMBER_OF_ELEVATORS-1)
+    whichfloor = randint(-1, 1)
+    possiblenewfloor = elevatorStates[whichelevatorIndex]["atFloor"]+whichfloor
+    if possiblenewfloor != 0 and possiblenewfloor <= NUMBER_OF_FLOORS:
+        elevatorStates[whichelevatorIndex]["atFloor"] = possiblenewfloor
     service.send_to_all(message=state)
     logging.info('broadcast message')
-    # result1 = yield context.call_activity('elevator', "Tokyo")
-    # result2 = yield context.call_activity('elevator', "Seattle")
-    # result3 = yield context.call_activity('elevator', "London")
+    response = elevatorsContainer.upsert_item(
+        body=elevatorStates[whichelevatorIndex])
     return ["nothing"]
 
 
