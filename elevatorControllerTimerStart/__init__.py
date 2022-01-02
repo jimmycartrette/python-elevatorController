eb4e3f2496bec8774123baac56ba13de70fb4a38
Jimmy Cartrette <jimmy.cartrette@gmail.com>
2022-01-01 23:17:27 -0500

    Clean up

--- elevatorControllerTimerStart/__init__.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-import azure.functions as func
-import azure.durable_functions as df
-
-
-def main(mytimer: func.TimerRequest) -> None:
-    url = os.environ['ELEVATORSTARTERURL']
-    r = requests.get(url)
